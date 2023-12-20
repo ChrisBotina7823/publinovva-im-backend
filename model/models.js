38316bf913a337913e5cf442d00602ced46fcdf2
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2023-12-19 22:57:02 -0500

    Implement shortIds for all entities

--- model/models.js
+++ model/models.js
@@ -1,4 +1,37 @@
 import mongoose from '../db/db-connection.js';
+import shortid from 'shortid'
+
+function generateEightDigitNumber() {
+    const min = 10000000; // Mínimo número de 8 dígitos
+    const max = 99999999; // Máximo número de 8 dígitos
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+const generateUniqueShortId = async (Model) => {
+    let isUnique = false;
+    let newShortId;
+
+    while (!isUnique) {
+        newShortId = generateEightDigitNumber();
+        const existingDocument = await Model.findOne({ shortId: newShortId });
+        isUnique = !existingDocument;
+    }
+
+    return newShortId;
+}
+
+const addShortIdToSchema = (schema) => {
+    schema.add({
+        shortId: { type: String, unique: true },
+    });
+
+    schema.pre('save', async function (next) {
+        if (!this.shortId) {
+            this.shortId = await generateUniqueShortId(this.constructor);
+        }
+        next();
+    });
+}
 
 // User Model
 const userSchema = new mongoose.Schema({
@@ -6,9 +39,10 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     email: { type: String, required: true },
     profile_picture: { type: String, default: "" },
-    recovery_token: { type:String, default: "" },
+    recovery_token: { type: String, default: "" },
 });
 
+addShortIdToSchema(userSchema);
 const User = mongoose.model('User', userSchema);
 
 // Admin Model (Inherits from User)
@@ -46,6 +80,7 @@ const packageSchema = new mongoose.Schema({
     admin: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin' }
 });
 
+addShortIdToSchema(packageSchema);
 const Package = mongoose.model('Package', packageSchema);
 
 // Wallet Model
@@ -58,17 +93,19 @@ const walletSchema = new mongoose.Schema({
     admin: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin' }
 });
 
+addShortIdToSchema(walletSchema);
 const Wallet = mongoose.model('Wallet', walletSchema);
 
 // Movement Model
 const movementSchema = new mongoose.Schema({
-    date: { type: Date, default: Date.now, required:true },
-    movement_state: { type: String, required:true, default: "pendiente", enum: ["pendiente", "aprobado", "rechazado", "remitido", "cancelado", "resuelto"] },
-    description: { type: String },  
+    date: { type: Date, default: Date.now, required: true },
+    movement_state: { type: String, required: true, default: "pendiente", enum: ["pendiente", "aprobado", "rechazado", "remitido", "cancelado", "resuelto"] },
+    description: { type: String },
     admin: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin' },
     client: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' },
 });
 
+addShortIdToSchema(movementSchema);
 const Movement = mongoose.model('Movement', movementSchema);
 
 // Wallet Transaction Model (Inherits from Movement)
@@ -95,13 +132,14 @@ const investmentSchema = new mongoose.Schema({
     actual_start_date: { type: Date },
     end_date: { type: Date, required: true },
     package: { type: mongoose.Schema.Types.ObjectId, ref: 'Package', required: true },
-    state: { type: String, required: true, default:"pendiente", enum: ["pendiente", "en curso", "rechazado", "finalizado"] },
-    client: { type: mongoose.Schema.Types.ObjectId, ref: "Client"},
-    wallet: { type: mongoose.Schema.Types.ObjectId, ref:"Wallet" },
+    state: { type: String, required: true, default: "pendiente", enum: ["pendiente", "en curso", "rechazado", "finalizado"] },
+    client: { type: mongoose.Schema.Types.ObjectId, ref: "Client" },
+    wallet: { type: mongoose.Schema.Types.ObjectId, ref: "Wallet" },
     inv_amount: { type: Number, default: 0 },
     revenue: { type: Number, default: 0 }
 });
 
+addShortIdToSchema(investmentSchema);
 const Investment = mongoose.model('Investment', investmentSchema);
 
 export {
